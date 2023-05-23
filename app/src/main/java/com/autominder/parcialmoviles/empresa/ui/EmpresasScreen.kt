164ca6fd337a4f38ac95853996e144733c1875a4
melvin <melvin@debian-BULLSEYE-live-builder-AMD64>
2023-05-23 09:46:49 -0600

    Fix: application wastn in the manifest

--- app/src/main/java/com/autominder/parcialmoviles/empresa/ui/EmpresasScreen.kt
+++ app/src/main/java/com/autominder/parcialmoviles/empresa/ui/EmpresasScreen.kt
@@ -1,11 +1,14 @@
 package com.autominder.parcialmoviles.empresa.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.autominder.parcialmoviles.components.FloatingAddButton
@@ -19,6 +22,7 @@ fun EmpresasScreen(
         floatingActionButton = { FloatingAddButton(navController) }
     ) {
         Box(modifier = Modifier.padding(it)){
+            Text(text = "HOLA")
             MainEmpresaScreen(empresaViewModel, navController)
         }
     }
