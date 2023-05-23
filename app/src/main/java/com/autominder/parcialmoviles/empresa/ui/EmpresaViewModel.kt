ceb62873cc5df48011bb0789b6607479e92551d9
melvin <melvin@debian-BULLSEYE-live-builder-AMD64>
2023-05-23 09:27:21 -0600

    Add: capa viewmodel

---
+++ app/src/main/java/com/autominder/parcialmoviles/empresa/ui/EmpresaViewModel.kt
@@ -0,0 +1,38 @@
+package com.autominder.parcialmoviles.empresa.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.autominder.parcialmoviles.EmpresaApplication
+import com.autominder.parcialmoviles.model.EmpresaDataModel
+import com.autominder.parcialmoviles.repository.EmpresaRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class EmpresaViewModel(
+    private val repository: EmpresaRepository
+) : ViewModel() {
+    private val _empresaList = MutableStateFlow<List<EmpresaDataModel>>(emptyList())
+    val empresaList: StateFlow<List<EmpresaDataModel>> = _empresaList
+    private val _isLoading = MutableStateFlow<Boolean>(false)
+    val isLoding: StateFlow<Boolean> = _isLoading
+
+
+    fun getEmpresas() {
+        viewModelScope.launch {
+            _empresaList.value = repository.getEmpresas()
+        }
+    }
+
+    companion object {
+        val Factory = viewModelFactory {
+            initializer {
+                val app = this[APPLICATION_KEY] as EmpresaApplication
+                EmpresaViewModel(app.empresaRepository)
+            }
+        }
+    }
+}
